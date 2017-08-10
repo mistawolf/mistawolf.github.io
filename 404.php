a5ac2f241ebb8e10a1fea717f81933318624e73a
mistawolf <30900733+mistawolf@users.noreply.github.com>
2017-08-10 14:04:29 +0000

    v1

---
+++ 404.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>404</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+
+  </head>
+  <body class="error">
+    <div class="container">
+      <div style="padding-top: 50px" class="col-lg-8 col-lg-offset-2 text-center">
+        <div class="logo">
+		<img src="/images/logodrwolf.xyz.bl.png" border="0">
+        </div>
+        <p class="lead text-muted">Requested file is not found.</p>
+        <div class="clearfix"></div>
+      </div><!-- /.col-lg-8 col-offset-2 -->
+    </div>
+  </body>
